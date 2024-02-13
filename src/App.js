f0ff485beadaeb5bd9d317a4373d6d796f5d0134
1543-STACK <141954178+1543-STACK@users.noreply.github.com>
2024-02-13 11:38:50 +0200

    added app.js and weather.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import "./App.css";
-
+import Weather from "./Weather";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Durban" />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/1543-STACK/final-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Anu Govender
+          </a>{" "}
+          and is {""}
+          <a
+            href="https://github.com/1543-STACK/final-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open sourced on Github {""}
+          </a>
+          and {""}
+          <a
+            href="https://sensational-basbousa-620fea.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
-
 export default App;
